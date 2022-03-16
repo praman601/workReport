0b5768edf80c5838bb9ea58f944de1e283f363d0
pradeep budhathoki <pradeep.budhathoki1234@gmail.com>
2022-03-16 12:49:56 +0545

    commit202203161249

--- workReport/Providers/Utility.cs
+++ workReport/Providers/Utility.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
 using System.Linq;
-using System.Net;
 using System.Web;
-using System.Web.Mvc;
 using workReport.Models;
+
+using System.Web.Mvc;
+
 using PagedList;
 using System.IO;
 using System.Configuration;
@@ -18,6 +19,8 @@ using System.Reflection;
 using ClosedXML.Excel;
 using OfficeOpenXml;
 using OfficeOpenXml.Style;
+using workReport.Controllers;
+
 namespace workReport.Providers
 {
     public class DashData{
@@ -25,16 +28,23 @@ namespace workReport.Providers
         public string anydesksCount { get; set; }
         public string emailsCount { get; set; }
     }
-    public class Utility
+    public class Utility 
     {
-        public static DashData getTodaysDataa(string todaysDate)
+        private readonly SessionCheckController _stt;
+        public Utility(SessionCheckController stt)
         {
+            _stt = stt;
+        }
+        public  DashData getTodaysDataa(string todaysDate)
+        {
+         
+            var av = HttpContext.Current.Session["userId"].ToString();
             int isuser = Convert.ToInt32(HttpContext.Current.Session["userId"]);
             DashData data = new DashData();
             DateTime isDate = Convert.ToDateTime(todaysDate);
             using (var db =new  workReportEntities()) { 
             var todaysData = db.workList.Where(x => x.date_Eng == isDate).ToList();
-       data.callsCount = todaysData.Where(x => x.workListType == "Call").Sum(x => x.time).ToString();
+                 data.callsCount = todaysData.Where(x => x.workListType == "Call").Sum(x => x.time).ToString();
                 data.anydesksCount = todaysData.Where(x => x.workListType == "Anydesk").Sum(x => x.time).ToString();
                 data.emailsCount = todaysData.Where(x => x.workListType == "Email").Sum(x => x.time).ToString();
       
